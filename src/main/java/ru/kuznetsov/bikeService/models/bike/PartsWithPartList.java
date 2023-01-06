25a99de4a41f283f31e5f044d16b42f440798dab
Pavel Kuznetsov <technic47@gmail.com>
2023-01-06 17:29:10 +0300

    Small refactor. Not checked.

--- src/main/java/ru/kuznetsov/bikeService/models/bike/PartsWithPartList.java
+++ src/main/java/ru/kuznetsov/bikeService/models/bike/PartsWithPartList.java
@@ -1,8 +1,9 @@
 package ru.kuznetsov.bikeService.models.bike;
 
+import com.google.gson.reflect.TypeToken;
 import org.springframework.stereotype.Component;
-import ru.kuznetsov.bikeService.models.JSONConverter;
 
+import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,25 +11,34 @@ import java.util.Map;
 public class PartsWithPartList extends SmallPart {
     protected final String partList;
 
-    protected final JSONConverter<Map<String, Integer>> converterPartList;
-
     public PartsWithPartList() {
-        this.converterPartList = new JSONConverter<>();
         Map<String, Integer> newPartList = new HashMap<>();
-        this.partList = converterPartList.toJson(newPartList);
+        this.partList = converter.toJson(newPartList);
     }
 
-    public Map<String, Integer> getPartList() {
-        return converterPartList.fromJson(this.partList);
+    public String getPartList() {
+        return this.partList;
+    }
+
+    private Map<String, Integer> returnPartListObject() {
+        Type type = new TypeToken<Map<String, Integer>>() {
+        }.getType();
+        return converter.fromJson(this.partList, type);
     }
 
     private void setPartList(Map<String, Integer> newPartList) {
-        this.serviceList = converterPartList.toJson(newPartList);
+        this.serviceList = converter.toJson(newPartList);
     }
 
     public void addToPartList(Serviceable item) {
-        Map<String, Integer> currentPartList = this.getPartList();
+        Map<String, Integer> currentPartList = this.returnPartListObject();
         currentPartList.put(item.getClass().getSimpleName(), item.getId());
+        this.setPartList(currentPartList);
+    }
+
+    public void delFromPartList(Serviceable item) {
+        Map<String, Integer> currentPartList = this.returnPartListObject();
+        currentPartList.remove(item.getClass().getSimpleName(), item.getId());
         this.setPartList(currentPartList);
     }
 }
