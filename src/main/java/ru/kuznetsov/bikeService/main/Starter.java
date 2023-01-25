88fa89f86c1361870350ec9457851930b0dd9a75
Pavel <Technic47@gmail.com>
2023-01-25 18:51:43 +0300

    unstable views recognition. 404 errors.

--- src/main/java/ru/kuznetsov/bikeService/main/Starter.java
+++ src/main/java/ru/kuznetsov/bikeService/main/Starter.java
@@ -3,12 +3,17 @@ package ru.kuznetsov.bikeService.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
 public class Starter extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Starter.class, args);
+    }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Starter.class);
     }
 }
