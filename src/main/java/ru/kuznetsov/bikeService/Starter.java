1ffd42394a46d16ffbe3004be861c54760a51c01
Pavel <Technic47@gmail.com>
2023-01-26 15:16:23 +0300

    Default security works

--- src/main/java/ru/kuznetsov/bikeService/Starter.java
+++ src/main/java/ru/kuznetsov/bikeService/Starter.java
@@ -2,11 +2,10 @@ package ru.kuznetsov.bikeService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
+@SpringBootApplication
 public class Starter extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Starter.class, args);
