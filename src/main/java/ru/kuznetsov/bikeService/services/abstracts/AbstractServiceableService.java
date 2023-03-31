27c5dd2a10c7beda650dd81075f565134671aef2
Pavel <Technic47@gmail.com>
2023-03-31 15:33:42 +0300

    quantity work refactored.

--- src/main/java/ru/kuznetsov/bikeService/services/abstracts/AbstractServiceableService.java
+++ src/main/java/ru/kuznetsov/bikeService/services/abstracts/AbstractServiceableService.java
@@ -26,18 +26,27 @@ public abstract class AbstractServiceableService<E extends AbstractServiceableEn
 
     public void addToLinkedItems(E item, PartEntity entity) {
         Set<PartEntity> entitySet = item.getLinkedItems();
+
         Optional<PartEntity> searchItem = entitySet.stream()
                 .parallel()
                 .filter(part -> part.equals(entity))
                 .findFirst();
+
         if (searchItem.isPresent()) {
-            PartEntity changeAmount = searchItem.get();
-            entitySet.remove(changeAmount);
-            changeAmount.setAmount(changeAmount.getAmount() + entity.getAmount());
-            entitySet.add(changeAmount);
+            switch (searchItem.get().getType()) {
+                case "Consumable", "Fastener":
+                    PartEntity changeAmount = searchItem.get();
+                    entitySet.remove(changeAmount);
+                    changeAmount.setAmount(changeAmount.getAmount() + entity.getAmount());
+                    entitySet.add(changeAmount);
+                    break;
+                case "Part", "Document", "Tool":
+                    break;
+            }
         } else {
             entitySet.add(entity);
         }
+
         repository.save(item);
     }
 
@@ -47,14 +56,20 @@ public abstract class AbstractServiceableService<E extends AbstractServiceableEn
                 .parallel()
                 .filter(part -> part.equals(entity))
                 .findFirst();
+
         if (searchItem.isPresent()) {
-            PartEntity changeAmount = searchItem.get();
-            if ((changeAmount.getAmount() - entity.getAmount()) <= 0) {
-                entitySet.remove(changeAmount);
-            } else {
-                entitySet.remove(changeAmount);
-                changeAmount.setAmount(changeAmount.getAmount() - entity.getAmount());
-                entitySet.add(changeAmount);
+            switch (searchItem.get().getType()) {
+                case "Consumable", "Fastener" -> {
+                    PartEntity changeAmount = searchItem.get();
+                    if ((changeAmount.getAmount() - entity.getAmount()) <= 0) {
+                        entitySet.remove(changeAmount);
+                    } else {
+                        entitySet.remove(changeAmount);
+                        changeAmount.setAmount(changeAmount.getAmount() - entity.getAmount());
+                        entitySet.add(changeAmount);
+                    }
+                }
+                case "Part", "Document", "Tool" -> entitySet.remove(entity);
             }
             repository.save(item);
         }
