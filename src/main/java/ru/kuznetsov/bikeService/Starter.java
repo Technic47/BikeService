41ffcd11af21d85e7950372b9810e35a315675c3
Pavel <Technic47@gmail.com>
2023-04-13 13:01:12 +0300

    quantity work refactored.

--- src/main/java/ru/kuznetsov/bikeService/Starter.java
+++ src/main/java/ru/kuznetsov/bikeService/Starter.java
@@ -11,7 +11,6 @@ public class Starter extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Starter.class, args);
     }
-
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(Starter.class);
