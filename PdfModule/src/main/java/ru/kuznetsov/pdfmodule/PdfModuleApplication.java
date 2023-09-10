1e05fb56cb9631d92eb080043ba36bf774420eab
Pavel <Technic47@gmail.com>
2023-09-10 20:36:43 +0300

    Start.

---
+++ PdfModule/src/main/java/ru/kuznetsov/pdfmodule/PdfModuleApplication.java
@@ -0,0 +1,13 @@
+package ru.kuznetsov.pdfmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfModuleApplication.class, args);
+	}
+
+}
---
+++ PdfModule/src/test/java/ru/kuznetsov/pdfmodule/PdfModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kuznetsov.pdfmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
