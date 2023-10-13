0c68662c94761eed0ee450f5fe8d159236ea6acd
Pavel <Technic47@gmail.com>
2023-10-13 11:33:05 +0300

    Started.

---
+++ MainResources/src/test/java/ru/bikeservice/mainresources/MainResourcesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bikeservice.mainresources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainResourcesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
